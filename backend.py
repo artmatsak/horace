fd1b20949cce7deca826fd739cf0d82a74f86d1f
Art Matsak <art@overta.lk>
2023-01-19 12:41:18 +0100

    Move backend functions to backend.py

---
+++ backend.py
@@ -0,0 +1,18 @@
+from router import Router
+
+backend = Router()
+
+domain = {
+    "business_name": "Death Star, a Star Wars-themed restaurant in Cupertino, CA",
+    "extra_instructions": "In your speech, you impersonate Jedi Master Yoda."
+}
+
+
+@backend.command(desc="book a table", example_params=("Jose James", 2, "next Friday", "6:00 pm"))
+def create_booking(name: str, num_people: int, date: str, time: str) -> str:
+    return "Booking successful, reference: YEHBZL"
+
+
+@backend.command(desc="cancel a booking", example_params=("HTLYNN"))
+def delete_booking(reference: str) -> str:
+    return "Booking canceled"
--- main.py
+++ main.py
@@ -3,29 +3,12 @@ import logging
 from colorama import Fore, Style
 # import mocks.openai as openai
 import openai
-from router import Router
+from backend import backend, domain
 from grace_chatbot import GRACEChatbot
 from dotenv import load_dotenv
 from typing import List
 
 load_dotenv()
-
-backend = Router()
-
-domain = {
-    "business_name": "Death Star, a Star Wars-themed restaurant in Cupertino, CA",
-    "extra_instructions": "In your speech, you impersonate Jedi Master Yoda."
-}
-
-
-@backend.command(desc="book a table", example_params=("Jose James", 2, "next Friday", "6:00 pm"))
-def create_booking(name: str, num_people: int, date: str, time: str) -> str:
-    return "Booking successful, reference: YEHBZL"
-
-
-@backend.command(desc="cancel a booking", example_params=("HTLYNN"))
-def delete_booking(reference: str) -> str:
-    return "Booking canceled"
 
 
 def print_utterances(utterances: List[str]):
