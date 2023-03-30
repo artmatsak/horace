a42dd0b7e165e829f06b7d528dbf0618f0a52cb5
Art Matsak <art@overta.lk>
2023-03-30 11:45:25 +0200

    Minor improvements to auth storage implementation

--- router.py
+++ router.py
@@ -12,72 +12,73 @@ from typing import List, Dict
 
 
 class Router():
-    PLUGIN_DB_FILENAME = '.plugin_db.json'
+    PLUGIN_AUTH_FILENAME = '.plugin_auth.json'
     AUTH_TYPE_NONE = "none"
     AUTH_TYPE_USER_HTTP = "user_http"
 
     def __init__(self, plugins: List[str]):
-        mode = 'r+' if os.path.isfile(self.PLUGIN_DB_FILENAME) else 'w'
-        with open(self.PLUGIN_DB_FILENAME, mode) as f:
-            plugin_db = json.load(f) if f.readable() else {}
+        if os.path.isfile(self.PLUGIN_AUTH_FILENAME):
+            with open(self.PLUGIN_AUTH_FILENAME, 'r') as f:
+                plugin_auth = json.load(f)
+        else:
+            plugin_auth = {}
 
-            self.registry = {}
-            plugin_db_update = {}
-            for netloc in set(plugins):
-                logging.debug(f"Loading plugin: {netloc}")
+        self.registry = {}
+        plugin_auth_update = {}
+        for netloc in set(plugins):
+            logging.debug(f"Loading plugin: {netloc}")
 
-                manifest_url = urlunsplit(
-                    ('http', netloc, '/.well-known/ai-plugin.json', '', ''))
-                response = requests.get(manifest_url)
-                response.raise_for_status()
-                manifest = json.loads(response.text)
+            manifest_url = urlunsplit(
+                ('http', netloc, '/.well-known/ai-plugin.json', '', ''))
+            response = requests.get(manifest_url)
+            response.raise_for_status()
+            manifest = json.loads(response.text)
 
-                if manifest["auth"]["type"] not in [self.AUTH_TYPE_NONE, self.AUTH_TYPE_USER_HTTP]:
-                    logging.debug(
-                        f'Plugin auth type not yet supported, skipping: {manifest["auth"]["type"]}')
-                    continue
+            if manifest["auth"]["type"] not in [self.AUTH_TYPE_NONE, self.AUTH_TYPE_USER_HTTP]:
+                logging.debug(
+                    f'Plugin auth type not yet supported, skipping: {manifest["auth"]["type"]}')
+                continue
 
-                if netloc not in plugin_db or manifest["auth"]["type"] != plugin_db[netloc]["auth_type"]:
-                    plugin_db_update[netloc] = {
-                        "auth_type": manifest["auth"]["type"]
-                    }
-
-                    if manifest["auth"]["type"] == self.AUTH_TYPE_USER_HTTP:
-                        plugin_db_update[netloc]["auth_token"] = input(
-                            f'Enter access token for {manifest["name_for_human"]}: ')
-                else:
-                    plugin_db_update[netloc] = plugin_db[netloc].copy()
-
-                response = requests.get(manifest["api"]["url"])
-                response.raise_for_status()
-                spec_yaml = response.text
-
-                self.registry[manifest["name_for_model"]] = {
-                    "manifest": manifest,
-                    "spec_yaml": spec_yaml,
-                    "db_info": plugin_db_update[netloc]
+            if netloc not in plugin_auth or manifest["auth"]["type"] != plugin_auth[netloc]["type"]:
+                plugin_auth_update[netloc] = {
+                    "type": manifest["auth"]["type"]
                 }
 
-                spec_dict = yaml.safe_load(spec_yaml)
-                try:
-                    self.registry[manifest["name_for_model"]]["spec"] = openapi_core.Spec.create(
-                        data=spec_dict)
-                except OpenAPIValidationError as error:
-                    logging.warning(
-                        f"Invalid OpenAPI specification for plugin {netloc}. Horace will be unable to validate LLM requests to this plugin against the spec. Invalid spec may also cause the LLM to form invalid requests.")
+                if manifest["auth"]["type"] == self.AUTH_TYPE_USER_HTTP:
+                    plugin_auth_update[netloc]["token"] = input(
+                        f'Enter access token for {manifest["name_for_human"]}: ')
+            else:
+                plugin_auth_update[netloc] = plugin_auth[netloc].copy()
 
-            f.seek(0)
-            json.dump(plugin_db_update, f)
-            f.truncate()
+            response = requests.get(manifest["api"]["url"])
+            response.raise_for_status()
+            spec_yaml = response.text
+
+            self.registry[manifest["name_for_model"]] = {
+                "manifest": manifest,
+                "spec_yaml": spec_yaml,
+                "auth": plugin_auth_update[netloc]
+            }
+
+            spec_dict = yaml.safe_load(spec_yaml)
+            try:
+                self.registry[manifest["name_for_model"]]["spec"] = openapi_core.Spec.create(
+                    data=spec_dict)
+            except OpenAPIValidationError as error:
+                logging.warning(
+                    f"Invalid OpenAPI specification for plugin {netloc}. Horace will be unable to validate LLM requests to this plugin against the spec. Invalid spec may also cause the LLM to form invalid requests.")
+
+        with open(self.PLUGIN_AUTH_FILENAME, 'w') as f:
+            json.dump(plugin_auth_update, f)
 
     def call(self, plugin_name: str, request_object_params: Dict) -> str:
         if plugin_name not in self.registry:
             raise ValueError(f"Unknown plugin: {plugin_name}")
 
         # Add authorization headers, if any
-        if self.registry[plugin_name]["db_info"]["auth_type"] == self.AUTH_TYPE_USER_HTTP:
+        if self.registry[plugin_name]["auth"]["type"] == self.AUTH_TYPE_USER_HTTP:
             extra_headers = {
-                'Authorization': f'Bearer {self.registry[plugin_name]["db_info"]["auth_token"]}'
+                'Authorization': f'Bearer {self.registry[plugin_name]["auth"]["token"]}'
             }
             if "headers" in request_object_params:
                 request_object_params["headers"].update(extra_headers)
