399d2196c08e78d5328b0244627d63e86d4ac298
Art Matsak <art@overta.lk>
2023-04-05 10:16:09 +0200

    Add bot typing indicator to the chat widget

--- client-demo/chat_widget.js
+++ client-demo/chat_widget.js
@@ -5,6 +5,8 @@ const chatForm = document.getElementById("chat-form");
 const chatInput = document.getElementById("chat-input");
 const chatSend = document.getElementById("chat-send");
 
+let typingBubble = null;
+
 socket.addEventListener("open", (event) => {
   console.log("WebSocket connection opened:", event);
 });
@@ -50,10 +52,12 @@ function handleState(state) {
     case "replying":
       chatInput.disabled = true;
       chatSend.disabled = true;
+      showTypingAnimation();
       break;
     case "listening":
       chatInput.disabled = false;
       chatSend.disabled = false;
+      hideTypingAnimation();
       break;
     case "ended":
       chatInput.disabled = true;
@@ -90,4 +94,32 @@ function displayError(message) {
   chatBubble.textContent = message;
   chatMessages.appendChild(chatBubble);
   chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+function showTypingAnimation() {
+  if (!typingBubble) {
+    typingBubble = document.createElement("div");
+    typingBubble.classList.add("chat-bubble");
+    typingBubble.classList.add("ai-message");
+
+    const typingIndicator = document.createElement("div");
+    typingIndicator.classList.add("typing-indicator");
+
+    for (let i = 0; i < 3; i++) {
+      const dot = document.createElement("span");
+      typingIndicator.appendChild(dot);
+    }
+
+    typingBubble.appendChild(typingIndicator);
+    chatMessages.appendChild(typingBubble);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+}
+
+function hideTypingAnimation() {
+  if (typingBubble) {
+    chatMessages.removeChild(typingBubble);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+    typingBubble = null;
+  }
 }
